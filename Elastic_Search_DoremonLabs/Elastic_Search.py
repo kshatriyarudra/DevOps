6ede8a71efa790e0ff86f52b1288e0bb0f633654
Rudra Pratap Singh <69968143+kshatriyarudra@users.noreply.github.com>
2022-02-14 22:20:51 +0530

    Add files via upload

--- Elastic_Search_DoremonLabs/Elastic_Search.py
+++ Elastic_Search_DoremonLabs/Elastic_Search.py
@@ -1,113 +1,101 @@
 # Importing required library
 
-from itertools import count
 import logging
 from elasticsearch import Elasticsearch
-import json
-import send_mail
+import send_email
 import query
 import config as cfg
 
-
-#configurating logging 
-logging.basicConfig(level=logging.INFO,filename='data.log',format='%(asctime)s %(levelname)s-%(message)s')
-
+# configurating logging
+logging.basicConfig(level=logging.INFO, filename='data.log', format='%(asctime)s %(levelname)s-%(message)s')
 
 try:
-# Give the elastic search address and authentication
-    es = Elasticsearch([cfg.details['host']], http_auth=(cfg.details['user'], cfg.details['passwd']))
+    # Give the elastic search address and authentication
+    es = Elasticsearch([cfg.elastic_search['host']],
+                       http_auth=(cfg.elastic_search['user'], cfg.elastic_search['passwd']))
 
 except Exception as e:
     logging.exception('Error while fetching information of elastic search')
 
-
 # Getting all the indices in res
-
-res=es.indices.get_alias(index=cfg.details['index'])
-if len(res)==0:
+res = es.indices.get_alias(index=cfg.elastic_search['index'])
+if len(res) == 0:
     logging.exception("Error occured while fetching index")
 
 
-def getindices(index,body):
+def get_data_of_index(index, body):
+    """Return datails of index after matched into the dict.
 
-    """This will return index after searching"""
+    Parameters:
+    index (str): Index will contain all data of particular index.
+    body (tuple): Body is query in which search is apply.
 
-    return es.search(index=index,body=body)
+    Returns:
+    tuple: After search according to the body in to the index data and then returns"""
+
+    return es.search(index=index, body=body)
 
 
+def count_of_doc_in_time_interval(res, body):
+    """Calculate the number of documents in given interval of time.
 
-#total_count_of_timestamp=0
-def count_of_doc_in_time_interval(res,body):
+    Parameters:
+    res (dict): Which include all index into the dict.
+    body (tuple): This is query which will pass into get_data_of_index.
 
-    """You are going to calculate the number of documents in given interval of time"""
+    Returns:
+    int: Total number of documents in given range of time."""
 
-    total_count_of_timestamp=0
-    
-    # this code will count the number of file from body
+    total_count_of_timestamp = 0
+
     for name in res:
-
-        data=getindices(name,body)
+        data = get_data_of_index(name, body)
         for data_timestamp in data['hits']['hits']:
-
-            data_time=data_timestamp['_source']['@timestamp']
-            #logging.info("---------------data_time----------------")
+            data_time = data_timestamp['_source']['@timestamp']
             logging.info(data_time)
-            total_count_of_timestamp+=1
+            total_count_of_timestamp += 1
 
     logging.info("Total counts in given range of time is--{}".format(total_count_of_timestamp))
-    logging.critical("Count for the documents in given time interval is Done!!!")
     return total_count_of_timestamp
 
 
-# this code will count the number of documents in given range of response time in body1(query)
-#total_count_of_response_time=0
-def count_of_doc_based_on_response_time(res,body1):
+def count_of_doc_based_on_response_time(res, body1):
+    """Calculate the number of documents in given response of time.
 
-    """You are going to calculate number of documents in given response time"""
+    Parameters:
+    res (dict): Which include all index into the dict.
+    body1 (tuple): This is query which will pass into get_data_of_index.
 
-    total_count_of_response_time=0
+    Returns:
+    int: Total number of documents in given response of time."""
+
+    total_count_of_response_time = 0
     for name in res:
 
-        data=getindices(name,body1)
+        data = get_data_of_index(name, body1)
         for data_response_time in data['hits']['hits']:
-
-            response_time=data_response_time['_source']['response_time']
+            response_time = data_response_time['_source']['response_time']
             logging.info(response_time)
-            total_count_of_response_time+=1
+            total_count_of_response_time += 1
 
-    logging.info("Total Counts in range of response_time<=--{}".format(total_count_of_response_time))
-    logging.critical("Count of documents in given range of response time is Done!!!")
+    logging.info("Total Counts in range of response_time<=- {}".format(total_count_of_response_time))
     return total_count_of_response_time
 
-#Calling function for count of documents in timestamp
+
+# Calling function for count of documents in timestamp
 try:
-    logging.warning(count_of_doc_in_time_interval.__doc__)
-    count_time = count_of_doc_in_time_interval(res,query.body)
+    count_time = count_of_doc_in_time_interval(res, query.body)
 
 except Exception as e:
     logging.exception("Error occured while calculating number of counts")
 
-
-#Calling function for count of documents in response time
+# Calling function for count of documents in response time
 try:
-    logging.warning(count_of_doc_based_on_response_time.__doc__)
-    count_response = count_of_doc_based_on_response_time(res,query.body1)
+    count_response = count_of_doc_based_on_response_time(res, query.body1)
 except:
     logging.exception("Error occured while calculating number of counts")
 
-
-def sending_email():
-
-    """You are going to send email"""
-
-    return send_mail.send(count_time,count_response)
-
 try:
-    logging.warning(sending_email.__doc__)
-    sending_email()
+    send_email.send(count_time, count_response)
 except Exception as e:
     logging.exception("Error while sending email")
-
-
-
-
--- Elastic_Search_DoremonLabs/config.py
+++ Elastic_Search_DoremonLabs/config.py
@@ -1,11 +1,13 @@
-details = {
+elastic_search = {
         "host":"http://10.0.25.190",
         "port" : 9200,
-        "user":"####",
-        "passwd":"####",
+        "user":"elastic_read",
+        "passwd":"elastic_read",
         "index" : "quest-staging-apache-accesslogs-*",
-        "start_date_time":"2022-02-12T04:59:30.000Z",
-        "end_date_time":"2022-02-12T05:00:00.000Z",
+        "start_date_time":"2022-02-10T06:59:30.000Z",
+        "end_date_time":"2022-02-14T05:00:00.000Z"
+}
+email_details={
         "from_address" : "rudra931592@gmail.com",
-        "email_password" : "####"
+        "email_password" : "doremonlabs"
     }
