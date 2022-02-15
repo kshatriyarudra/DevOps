807956b6b5b02563ed2426747f310a8f817ae1ae
Rudra Pratap Singh <69968143+kshatriyarudra@users.noreply.github.com>
2022-02-15 17:15:59 +0530

    Update config.py
    Complete

--- Elastic_Search_DoremonLabs/config.py
+++ Elastic_Search_DoremonLabs/config.py
@@ -1,13 +1,13 @@
 elastic_search = {
         "host":"http://10.0.25.190",
         "port" : 9200,
-        "user":"elastic_read",
-        "passwd":"elastic_read",
+        "user":"####",
+        "passwd":"####",
         "index" : "quest-staging-apache-accesslogs-*",
         "start_date_time":"2022-02-10T06:59:30.000Z",
         "end_date_time":"2022-02-14T05:00:00.000Z"
 }
 email_details={
         "from_address" : "rudra931592@gmail.com",
-        "email_password" : "doremonlabs"
+        "email_password" : "####"
     }
